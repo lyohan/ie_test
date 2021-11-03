247c149a047e145bc5115abdde5a6bef4a01fc5f
LeeYoHan <yohan4551@gmail.com>
2021-11-03 17:03:47 +0900

    feat: add react polyfill

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,5 @@
+import "react-app-polyfill/ie11";
+import "react-app-polyfill/stable";
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { ToastContainer } from 'react-toastify'
