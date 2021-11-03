f412d80bdc8bada80238769be43bdd8c2854a318
LeeYoHan <yohan4551@gmail.com>
2021-11-03 17:26:46 +0900

    feat: change next version

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,10 @@
 import "react-app-polyfill/ie11";
 import "react-app-polyfill/stable";
+import 'react-toastify/dist/ReactToastify.css'
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { ToastContainer } from 'react-toastify'
-import 'react-toastify/dist/ReactToastify.css'
+
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
